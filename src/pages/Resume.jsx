d79c1191de2e8010bfacb02d084d93e6c9b6a169
Samuel Amagbakhen <frontend_dev1@academystack.com>
2022-04-03 00:31:21 +0100

    main: main corrections to the projects section.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,19 @@
-function Resume (){
+import {useLocation} from "react-router-dom";
+import {useContext, useEffect} from "react";
+import NavigationContext from "../context/NavigationContext/NavigationContext";
 
+function Resume (){
+    const location = useLocation().pathname
+    const{pathName, dispatch} = useContext(NavigationContext);
+
+    useEffect(()=>{
+        dispatch({
+            type:"CHECK_PATHNAME",
+            payload: location
+        })
+    },[dispatch, location])
     return(
-        <h1>Resume</h1>
+        <h1>Resum</h1>
     )
 }
 
