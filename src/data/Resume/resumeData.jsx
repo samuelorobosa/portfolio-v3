915beab4107e9763283200344a7725de3655491d
Samuel Amagbakhen <frontend_dev1@academystack.com>
2022-04-03 23:10:17 +0100

    main: included the resume file

---
+++ src/data/Resume/resumeData.jsx
@@ -0,0 +1,53 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const resumeData = {
+        name: "Samuel",
+        surname: "Amagbakhen",
+        role: "Frontend Developer",
+        description: "A creative, ambitious, and self-motivated frontend developer with a passion for creating things that live and breathe on the web. I can work with popular front-end technologies, utilizing and these tools to deliver a pixel-perfect design and user-centered implementation with proper documentation of my implementation.",
+        toolsData: [
+            {
+                id: uuidv4(),
+                title: "Core Technologies",
+               data: ["HTML/CSS/SCSS", "JavaScript(ES6+)"]
+            },
+            {
+                id: uuidv4(),
+                title: "Libraries and Frameworks",
+                data: ["Bootstrap", "ReactJS", "jQuery", "TailwindCSS", "Laravel Blade Templates"]
+            },
+            {
+                id: uuidv4(),
+                title: "Tools and Platforms",
+                data: ["Git/GitHub/BitBucket", "Jira", "WordPress", "Vercel", "Netlify", "Confluence"]
+            },
+            {
+                id: uuidv4(),
+                title: "Design",
+                data: ["Figma","InVision"]
+            },
+        ],
+        experience: [
+            {
+                jobTitle: "Frontend Developer",
+                company: "Edo State Skills Development Agency",
+                startDate: "May 2020",
+                endDate:  "July 2021", //leave blank if still working
+                workDone: [
+                    "Implemented component-based coding pattern using ReactJs in order to improve page speed and load time",
+                    "Designed and implemented promotional landing pages to boost marketing for the government programs ensuring a wide reach",
+                    "Designed and implemented promotional landing pages to boost marketing for the government programs ensuring a wide reach"
+                ]
+            },
+            {
+                jobTitle: "Frontend Developer[Intern]",
+                company: "HNG Internship",
+                startDate: "May 2020",
+                endDate:  "August 2020", //leave blank if still working
+                workDone: [
+                    "Collaborated closely with the Front End team to deliver pixel-perfect landing pages from Figma Designs which were compiled and sold as themes on ThemeForest",
+                    "Developed and maintained websites utilizing HTML5, CSS 3, Bootstrap 4, JavaScript, jQuery, AJAX, and PHP to learn how to send and receive data between client and server",
+                ]
+            }
+        ]
+}
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,10 +3,14 @@ import {useContext, useEffect} from "react";
 import NavigationContext from "../context/NavigationContext/NavigationContext";
 import { ImDownload } from "react-icons/im";
 import './styles.scss'
+import {resumeData} from "../data/Resume/resumeData";
+import { v4 as uuidv4 } from 'uuid';
+import Pdf from "../assets/resume/Resume.pdf";
 
 function Resume (){
     const location = useLocation().pathname
     const{pathName, dispatch} = useContext(NavigationContext);
+    const{name, surname, role,description,toolsData,experience} = resumeData;
 
     useEffect(()=>{
         dispatch({
@@ -24,113 +28,64 @@ function Resume (){
 
            <div className="resume-container p-5 mt-10  w-11/12 md:w-1/2 mx-auto flex flex-col bg-main-color rounded">
                <div className="top_row flex justify-between items-stretch">
-                   <h1 className="text-2xl font-semibold font-family-epilogue">Samuel Amagbakhen</h1>
-                   <Link to={'/'} className="stand-out-color flex items-center"> Download &nbsp;<ImDownload /> </Link>
+                   <h1 className="text-2xl font-semibold font-family-epilogue">{`${name} ${surname}`}</h1>
+                   <a href={Pdf} target="_blank" className="stand-out-color flex items-center"> Download &nbsp;<ImDownload /> </a>
                </div>
-               <h2 className="text-base stand-out-color">Frontend Developer</h2>
+               <h2 className="text-base stand-out-color">{role}</h2>
                <div className="flex my-7">
-                   <p>
-                       A creative, ambitious, and self-motivated frontend developer with a passion for creating things that live and breathe on the web.
-                       I can work with popular front-end technologies, utilizing and these tools to deliver a pixel-perfect design and user-centered implementation with proper documentation of my implementation.
-                   </p>
+                   <p>{description}</p>
                </div>
-               <div className="divider">
-                   <p className="font-bold secondary-text-color mr-1">Core Technologies</p>
-                   <span/>
-               </div>
-               <div className="flex my-1">
-                   <p>
-                       HTML/CSS/SCSS, JavaScript(ES6+)
-                   </p>
-               </div>
-               <div className="divider">
-                   <p className="font-bold secondary-text-color mr-1">Libraries and Frameworks</p>
-                   <span/>
-               </div>
-               <div className="flex my-1">
-                   <p>
-                       Bootstrap, ReactJS, jQuery, TailwindCSS, Laravel Blade Templates.
-                   </p>
-               </div>
-               <div className="divider">
-                   <p className="font-bold secondary-text-color mr-1">Tool and Platforms</p>
-                   <span/>
-               </div>
-               <div className="flex my-1">
-                   <p>
-                       Git/GitHub/BitBucket, Jira, WordPress, Vercel, Netlify, Confluence.
-                   </p>
-               </div>
+               {
+                   toolsData.map(({title, data}, index)=>{
+                       return(
+                           <div key={index}>
+                               <div className="divider">
+                                   <div className="font-bold secondary-text-color mr-1">{title}</div>
+                                   <span/>
+                               </div>
+                               <div className="flex my-1">
+                                   <div>
+                                       {data.map((item, index,arr)=>{
+                                           return(
+                                               item === arr[arr.length-1] ? `${item}.` : `${item}, `
+                                           )
+                                       })}
+                                   </div>
+                               </div>
+                           </div>
 
-               <div className="divider">
-                   <p className="font-bold secondary-text-color mr-1">Design</p>
-                   <span/>
-               </div>
-               <div className="flex my-1">
-                   <p>
-                       Figma, InVision
-                   </p>
-               </div>
+                       )
+                   })
+               }
                <div className="divider">
                    <p className="font-bold secondary-text-color mr-1">Work Experience</p>
                    <span/>
                </div>
                <div className="experience_wrapper">
-                   <div className="experience mb-5">
-                       <p className="title">
-                           <span className="font-bold">Frontend Developer</span>
-                           <span className="font-semibold">@</span>
-                           <span className="font-semibold stand-out-color">Edo State Skills Development Agency</span>
-                           <span className="block font-mono mt-px">
-                                MAY 2021 - JULY 2021
-                           </span>
-                       </p>
-                       <p className="description mt-3">
-                           <p className="work flex my-1">
-                               <span>■</span>
-                               <span className="w-1/12"/>
-                               Implemented component-based coding pattern using ReactJs in order
-                               to improve page speed and load time.
-                           </p>
-                           <p className="work flex my-1">
-                               <span>■</span>
-                               <span className="w-1/12"/>
-                               Designed and implemented promotional landing pages to boost
-                               marketing for the government programs ensuring a wide reach.
-                           </p>
-                           <p className="work flex my-1">
-                               <span>■</span>
-                               <span className="w-1/12"/>
-                               Designed and implemented promotional landing pages to boost
-                               marketing for the government programs ensuring a wide reach.
-                           </p>
-                       </p>
-                   </div>
-                   <div className="experience mb-5">
-                       <p className="title">
-                           <span className="font-bold">Frontend Developer[Intern]</span>
-                           <span className="font-semibold">@</span>
-                           <span className="font-semibold stand-out-color">HNG Internship</span>
-                           <span className="block font-mono mt-px">
-                                MAY 2020 - AUGUST 2020
-                           </span>
-                       </p>
-                       <p className="description mt-3">
-                           <p className="work flex my-1">
-                               <span>■</span>
-                               <span className="w-1/12"/>
-                               Collaborated closely with the Front End team to deliver pixel-perfect
-                               landing pages from Figma Designs which were compiled and sold as
-                               themes on ThemeForest.
-                           </p>
-                           <p className="work flex my-1">
-                               <span>■</span>
-                               <span className="w-1/12"/>
-                               Developed and maintained websites utilizing HTML5, CSS 3, Bootstrap
-                               4, JavaScript, jQuery, AJAX, and PHP to learn how to send and receive data between client and server.
-                           </p>
-                       </p>
-                   </div>
+                   {experience.map(({jobTitle, company, startDate, endDate, workDone}, index)=>{
+                       return(
+                           <div className="experience mb-5" key={uuidv4()}>
+                               <div className="title">
+                                   <span className="font-bold">{jobTitle}</span>
+                                   <span className="font-semibold">@</span>
+                                   <span className="font-semibold stand-out-color">{company}</span>
+                                   <span className="block font-mono mt-px">{startDate.toUpperCase()} - {endDate ? endDate.toUpperCase():"Present".toUpperCase()}</span>
+                               </div>
+                               <div className="description mt-3">
+                                   {workDone.map((item,index)=>{
+                                      return(
+                                          <div key={index} className="work flex my-1">
+                                           <span>■</span>
+                                           <span className="w-1/12"/>
+                                              {item}.
+                                       </div>
+                                       )
+                                   })}
+
+                               </div>
+                           </div>
+                       )
+                   })}
                </div>
                <div className="divider">
                    <p className="font-bold secondary-text-color mr-1">Projects</p>
@@ -139,9 +94,10 @@ function Resume (){
                <p className="work flex my-1">
                    <span>■</span>
                    <span className="w-1/12"/>
-                   <p> Links to my works can be found on <a className="stand-out-color hover-secondary-text-color" href="https://samuelorobosa.me/projects">samuelorobosa.me/projects </a>
+                   <span>
+                       Links to my works can be found on <a className="stand-out-color hover-secondary-text-color" href="https://samuelorobosa.me/projects">samuelorobosa.me/projects </a>
                        and more details can be provided upon request.
-                   </p>
+                   </span>
 
                </p>
            </div>
