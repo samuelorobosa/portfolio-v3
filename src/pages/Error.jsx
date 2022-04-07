6488b67943e29b589a2cce150a7e4ae737120396
Samuel Amagbakhen <frontend_dev1@academystack.com>
2022-04-07 08:47:59 +0100

    Included error page.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,21 @@
+import {Link} from "react-router-dom";
+
 function Error () {
   return (
-    <div>
-      <h1>Error</h1>
-      <p>
-        Sorry, something went wrong.
+    <div className="text-center mt-12 py-9">
+      <h1 className="text-8xl font-extrabold font-family-epilogue mb-9">Oops!</h1>
+      <p className="font-mono">
+        Something's missing.
       </p>
+        <p className="font-mono mb-9">
+        The page you're looking for does not exist or might have been removed but allow me to guide you.
+      </p>
+
+      <Link to="/">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Go Home
+            </button>
+      </Link>
     </div>
   )
 }
