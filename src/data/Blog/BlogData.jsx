0dfe203f6f7103d130fc5f0521aa6a8d331e289a
Samuel Amagbakhen <frontend_dev1@academystack.com>
2022-04-10 22:42:59 +0100

    main:minor updates

--- src/data/Blog/BlogData.jsx
+++ src/data/Blog/BlogData.jsx
@@ -24,7 +24,7 @@ function BlogData(){
 
     return(
         <div className="blogDivContainer px-5 md:px-24 mt-5 pb-44">
-            <ImSpinner9 className={`animate-spin text-5xl stand-out-color ${loading ? "":"hidden"}`}/>
+            <ImSpinner9 className={`absolute top-60 loadingIcon animate-spin text-5xl stand-out-color ${loading ? "":"hidden"}`}/>
             {blogs.map(({id,title,published_at,tags,canonical_url})=>{
                 const convertedDate = new Date(published_at).toLocaleDateString(undefined,{
                     day: 'numeric',
@@ -32,7 +32,6 @@ function BlogData(){
                     year: 'numeric'
                 });
                 return (
-                    <>
                         <div key={id} className="flex flex-col justify-between w-full blogDiv opacity-0 p-4 mx-1 rounded my-3.5 text-left bg-secondary-text-color main-text-color">
                             <div>
                                 <h1 className="font-semibold font-family-epilogue text-lg">{title}</h1>
@@ -44,7 +43,6 @@ function BlogData(){
                                 <p className="text-sm secondary-text-color-lighter">#{tags}.</p>
                             </div>
                         </div>
-                    </>
 
                 )
             })}
